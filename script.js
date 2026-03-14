1e88a249c042f6d085a720c33b8945e4e494a78d
Aryan Bharadwaj <bharadwajaryan1@gmail.com>
2026-03-15 00:08:13 +0530

    Api Key added and tested

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
+//Api Key
+const API_KEY = "" //Tmdb Api key
+const BASE_URL = "https://api.themoviedb.org/3"
+const IMG_URL = "https://image.tmdb.org/t/p/w500"
+
+
 const movieGrid = document.getElementById("movieGrid")
 const trendingMovies = document.getElementById("trendingMovies")
 const recentMovies = document.getElementById("recentMovies")
 
+//Trending Movie Fetch
+async function fetchTrendingMovies(){
 
+const res = await fetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`)
 
+const data = await res.json()
+
+displayMovies(data.results, trendingMovies)
+
+}
+
+//Ratings function
 function averageRating(arr){
 
 if(arr.length===0) return 0
@@ -12,8 +28,19 @@ return (arr.reduce((a,b)=>a+b)/arr.length).toFixed(1)
 
 }
 
+//Latest Release
+async function fetchRecentMovies(){
+
+const res = await fetch(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}`)
+
+const data = await res.json()
+
+displayMovies(data.results, recentMovies)
+
+}
 
 
+//Create Star
 function createStars(index){
 
 let stars=""
@@ -34,25 +61,22 @@ return stars
 
 }
 
+//Movie Display
+function displayMovies(movies, target){
 
+target.innerHTML = ""
 
-function renderMovies(list,target){
-
-target.innerHTML=""
-
-list.forEach((movie,index)=>{
-
-const avg = averageRating(movie.ratings)
+movies.forEach(movie => {
 
 const card = document.createElement("div")
 
-card.className="bg-gray-800 rounded-lg shadow-lg overflow-hidden hover:scale-105 transition"
+card.className =
+"bg-gray-800 rounded-lg shadow-lg overflow-hidden hover:scale-105 transition"
 
-card.innerHTML=`
+card.innerHTML = `
 
 <img
-src="${movie.poster}"
-loading="lazy"
+src="${IMG_URL + movie.poster_path}"
 class="h-64 w-full object-cover"
 />
 
@@ -62,23 +86,13 @@ class="h-64 w-full object-cover"
 ${movie.title}
 </h3>
 
-<p class="text-sm text-gray-400">
-${movie.genre} • ${movie.year}
-</p>
-
 <p class="text-yellow-400">
-⭐ ${avg}
+⭐ ${movie.vote_average}
 </p>
 
-<div class="flex gap-1 mt-2">
-${createStars(index)}
-</div>
-
-<button
-onclick="openModal(${index})"
-class="mt-3 bg-red-500 px-3 py-1 rounded text-sm">
-View More
-</button>
+<p class="text-sm text-gray-400">
+${movie.release_date}
+</p>
 
 </div>
 `
@@ -89,16 +103,53 @@ target.appendChild(card)
 
 }
 
+//Search Movie
+async function searchMovies(){
+
+const query = document.getElementById("searchInput").value
+
+if(query.length < 1) return
+
+const res = await fetch(
+`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`
+)
+
+const data = await res.json()
+
+displayMovies(data.results, movieGrid)
+
+}
+
+//Famous Movie
+async function fetchPopularMovies(){
+
+const res = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
+
+const data = await res.json()
+
+displayMovies(data.results, movieGrid)
+
+}
+document
+.getElementById("searchBtn")
+.addEventListener("click", async () => {
+
+const query = document.getElementById("searchInput").value
+
+if(query.length < 1) return
+
+const res = await fetch(
+`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`
+)
+
+const data = await res.json()
+
+displayMovies(data.results, movieGrid)
+
+})
 
 
-renderMovies(movies,movieGrid)
-
-renderMovies(movies.filter(m=>m.trending),trendingMovies)
-
-renderMovies(movies.filter(m=>m.recent),recentMovies)
-
-
-
+//Movie Ratings
 function rateMovie(index,star){
 
 movies[index].ratings.push(star)
@@ -108,7 +159,7 @@ renderMovies(movies,movieGrid)
 }
 
 
-
+//PopUp
 function openModal(index){
 
 const m = movies[index]
@@ -135,23 +186,23 @@ document.getElementById("movieModal").classList.add("hidden")
 }
 
 
-
+//Search Movie
 document
 .getElementById("searchInput")
-.addEventListener("input",(e)=>{
+.addEventListener("keyup", async (e)=>{
 
-const value = e.target.value.toLowerCase()
+const query = e.target.value
 
-const filtered = movies.filter(movie=>
+if(query.length < 1) return
 
-movie.title.toLowerCase().includes(value) ||
-movie.genre.toLowerCase().includes(value) ||
-movie.cast.toLowerCase().includes(value) ||
-movie.year.toString().includes(value)
-
+const res = await fetch(
+`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`
 )
 
-renderMovies(filtered,movieGrid)
+const data = await res.json()
+
+// displayMovies(data.results)
+displayMovies(data.results, movieGrid)
 
 })
 
@@ -165,3 +216,9 @@ document.getElementById("ratingFilter").value = "";
 
 renderMovies(); 
 }
+
+
+
+fetchTrendingMovies()
+fetchRecentMovies()
+fetchPopularMovies()
